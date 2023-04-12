dc8d055214190c09d238c9106c5aaef6947c042b
Sidnei Henrique <henrisrs@hotmail.com>
2023-04-12 20:29:55 -0300

    Cadastro de funcionários e gerentes funcionando, e listagem de gerentes e funcionários funcionando

--- pbl_4/src/Main.java
+++ pbl_4/src/Main.java
@@ -1,12 +1,24 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Pessoa> pessoas = new ArrayList<>();
+        ArrayList<Gerente> gerentes = new ArrayList<>();
+        ArrayList<Funcionario> funcionarios = new ArrayList<>();
+        ArrayList<Departamento> departamentos = new ArrayList<>();
 
+        Random random = new Random();
         Scanner input = new Scanner(System.in);
+
+        departamentos.add(new Departamento(01, "Departamento de RH"));
+        departamentos.add(new Departamento(02, "Departamento de informática"));
+
+
+        gerentes.add(new Gerente("Pedro da Silva", 30, "444-444-444-55", "Masculino", 01, departamentos.get(0) ));
+        gerentes.add(new Gerente("Gabriel Felipe", 19, "554-443-444-55", "Masculino", 02, departamentos.get(1) ));
+
 
         while (true) {
             int selecaoMenuPrincipal = 0;
@@ -31,24 +43,105 @@ public class Main {
                     System.out.println("Digite sua idade: ");
                     int idade = input.nextInt();
                     System.out.println("Digite seu CPF: ");
-                    String cpf = input.nextString();
+                    String cpf = input.next();
                     System.out.println("Digite seu sexo: ");
                     System.out.println("1 - Masculino ");
                     System.out.println("2 - Feminino ");
                     System.out.println("3 - Outro ");
-                    int sexo = input.nextInt();
-                    System.out.println("Selecione o gerente responsável deste funcionário: ");
-                    System.out.println("");
+                    int sexoSelection = input.nextInt();
+                    String sexo;
 
+                    if(sexoSelection == 1){
+                        sexo = "Masculino";
+                    }
+
+                    else if(sexoSelection == 2) {
+                        sexo = "Feminino";
+                    }
+
+                    else {
+                        sexo = "Outro";
+                    }
+
+                    System.out.println("Selecione o gerente responsável deste funcionário: ");
+
+                    int i = 0;
+
+                    for (Gerente gerente : gerentes) {
+                            System.out.println( gerentes.indexOf(gerente) + " - " + gerente.getNome());
+                    }
+
+                    int gerenteSelected = input.nextInt();
+                    Gerente gerente = gerentes.get(gerenteSelected);
+
+                    int codigoFuncionario = random.nextInt();
+
+                    funcionarios.add(new Funcionario(nome, idade, cpf, sexo, codigoFuncionario, gerente));
+                    System.out.println("Funcionário " + nome + " cadastrado com sucesso");
 
                 }
 
                 else {
+                    System.out.println("Digite seu nome: ");
+                    String nome = input.next();
+                    System.out.println("Digite sua idade: ");
+                    int idade = input.nextInt();
+                    System.out.println("Digite seu CPF: ");
+                    String cpf = input.next();
+                    System.out.println("Digite seu sexo: ");
+                    System.out.println("1 - Masculino ");
+                    System.out.println("2 - Feminino ");
+                    System.out.println("3 - Outro ");
+                    int sexoSelection = input.nextInt();
+
+                    String sexo;
+
+                    if(sexoSelection == 1){
+                        sexo = "Masculino";
+                    }
+
+                    else if(sexoSelection == 2) {
+                        sexo = "Feminino";
+                    }
+
+                    else {
+                        sexo = "Outro";
+                    }
+                    System.out.println("Selecione o departamento do gerente: ");
+                    for (Departamento departamento : departamentos){
+                        System.out.println(departamentos.indexOf(departamento) + " - " + departamento.getNomeDepartamento());
+                    }
+
+                    int departamentoGerenteSelected = input.nextInt();
+                    Departamento departamentoGerente = departamentos.get(departamentoGerenteSelected);
+
+                    int codigoGerente = random.nextInt();
+
+                    gerentes.add(new Gerente(nome, idade, cpf, sexo, codigoGerente, departamentoGerente));
+                    System.out.println("Gerente " + nome + " cadastrado com sucesso");
 
                 }
 
 
             }
+
+            else if (selecaoMenuPrincipal == 2){
+
+                System.out.println("Gerentes no sistema: ");
+                System.out.println(" ");
+                for ( Gerente gerente : gerentes ){
+                    System.out.println( gerente.getCodigo() + " - " + gerente.getNome() + "," + gerente.getIdade() + " ;" + " Departamento: " + gerente.getDepartamentoResponsavel().getNomeDepartamento());
+                    System.out.println(" ");
+                }
+
+                System.out.println("Funcionários do sistema: ");
+                System.out.println(" ");
+                for(Funcionario funcionario : funcionarios){
+                    System.out.println(funcionario.getCodigoFuncionario() + " - " + funcionario.getNome() + "," + funcionario.getIdade() + " ;" + " Gerente responsável: " + funcionario.getGerenteResponsavel().getNome());
+                    System.out.println(" ");
+                }
+            }
+
 
 
 
